fe1431cf2420cc361e327a608f4880ed1b41d48c
fokla <hhajriwnko@gmain.com>
2024-03-31 16:50:35 +0530

    first commint in profile-1

---
+++ profile.js
@@ -0,0 +1,39 @@
+// to use buttons to show the content
+var heloo =
+  "Lorem ipsum dolor sit amet consecteturadipisicing elit. Neque accusantium, impedit iure soluta officia accusamus laboriosam perferendis amet minus quibusdam nulla corrupti perspiciatis officiis unde labore voluptatibus inventore dignissimos quo! Corrupti doloremque officiis a quia esse dolorum praesentium impedit! Expedita eligendi deserunt doloremque tenetur quia laudantium animi esse inventore provident perferendis culpa aliquam molestias facere a quod ratione quibusdam impedit possimus, aliquid nesciunt praesentium! Error ullam labore nulla sapiente harum ipsa voluptate illum pariatur, vitae blanditiis! Earum quisquam neque autem nostrum officiis nulla consequuntur quo sapiente consectetur illum. Illo quibusdam repudiandae provident dolor odio, perferendis magnam fuga at tenetur. Exercitationem.";
+var hii =
+  "Horem ipsum dolor sit amet consecteturadipisicing elit. Neque accusantium, impedit iure soluta officia accusamus laboriosam perferendis amet minus quibusdam nulla corrupti perspiciatis officiis unde labore voluptatibus inventore dignissimos quo! Corrupti doloremque officiis a quia esse dolorum praesentium impedit! Expedita eligendi deserunt doloremque tenetur quia laudantium animi esse inventore provident perferendis culpa aliquam molestias facere a quod ratione quibusdam impedit possimus, aliquid nesciunt praesentium! Error ullam labore nulla sapiente harum ipsa voluptate illum pariatur, vitae blanditiis! Earum quisquam neque autem nostrum officiis nulla consequuntur quo sapiente consectetur illum. Illo quibusdam repudiandae provident dolor odio, perferendis magnam fuga at tenetur. Exercitationem.";
+
+var tensorr =
+  "Torem ipsum dolor sit amet consecteturadipisicing elit. Neque accusantium, impedit iure soluta officia accusamus laboriosam perferendis amet minus quibusdam nulla corrupti perspiciatis officiis unde labore voluptatibus inventore dignissimos quo! Corrupti doloremque officiis a quia esse dolorum praesentium impedit! Expedita eligendi deserunt doloremque tenetur quia laudantium animi esse inventore provident perferendis culpa aliquam molestias facere a quod ratione quibusdam impedit possimus, aliquid nesciunt praesentium! Error ullam labore nulla sapiente harum ipsa voluptate illum pariatur, vitae blanditiis! Earum quisquam neque autem nostrum officiis nulla consequuntur quo sapiente consectetur illum. Illo quibusdam repudiandae provident dolor odio, perferendis magnam fuga at tenetur. Exercitationem.";
+
+var floww =
+  "Forem ipsum dolor sit amet consecteturadipisicing elit. Neque accusantium, impedit iure soluta officia accusamus laboriosam perferendis amet minus quibusdam nulla corrupti perspiciatis officiis unde labore voluptatibus inventore dignissimos quo! Corrupti doloremque officiis a quia esse dolorum praesentium impedit! Expedita eligendi deserunt doloremque tenetur quia laudantium animi esse inventore provident perferendis culpa aliquam molestias facere a quod ratione quibusdam impedit possimus, aliquid nesciunt praesentium! Error ullam labore nulla sapiente harum ipsa voluptate illum pariatur, vitae blanditiis! Earum quisquam neque autem nostrum officiis nulla consequuntur quo sapiente consectetur illum. Illo quibusdam repudiandae provident dolor odio, perferendis magnam fuga at tenetur. Exercitationem.";
+
+var downn =
+  "Dorem ipsum dolor sit amet consecteturadipisicing elit. Neque accusantium, impedit iure soluta officia accusamus laboriosam perferendis amet minus quibusdam nulla corrupti perspiciatis officiis unde labore voluptatibus inventore dignissimos quo! Corrupti doloremque officiis a quia esse dolorum praesentium impedit! Expedita eligendi deserunt doloremque tenetur quia laudantium animi esse inventore provident perferendis culpa aliquam molestias facere a quod ratione quibusdam impedit possimus, aliquid nesciunt praesentium! Error ullam labore nulla sapiente harum ipsa voluptate illum pariatur, vitae blanditiis! Earum quisquam neque autem nostrum officiis nulla consequuntur quo sapiente consectetur illum. Illo quibusdam repudiandae provident dolor odio, perferendis magnam fuga at tenetur. Exercitationem.";
+
+var i = 0;
+var speed = 10;
+//for helo
+window.onload = helo();
+function helo() {
+  document.getElementById("content").innerHTML = heloo;
+}
+//for hi
+function hi() {
+  document.getElementById("content").innerHTML = hii;
+}
+
+//for tensor
+function tensor() {
+  document.getElementById("content").innerHTML = tensorr;
+}
+//for flow
+function flow() {
+  document.getElementById("content").innerHTML = floww;
+}
+//for down
+function down() {
+  document.getElementById("content").innerHTML = downn;
+}
